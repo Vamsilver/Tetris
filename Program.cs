b199925adfb7c57acbc214a123314a82a33b617e
Vamsilver <vamsilver145@gmail.com>
2026-07-31 12:54:00 +0300

    Добавлены завершение и перезапуск игры

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Tetris;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.CursorVisible = false;
+
+try
+{
+    do
+    {
+        var game = new TetrisGame();
+        game.Run();
+
+        Console.SetCursorPosition(0, GameField.Height + 5);
+        Console.Write("Игра окончена! Нажмите R, чтобы начать заново, или Esc для выхода. ");
+
+        ConsoleKey key;
+        do
+        {
+            key = Console.ReadKey(true).Key;
+        }
+        while (key is not (ConsoleKey.R or ConsoleKey.Escape));
+
+        if (key == ConsoleKey.Escape)
+            break;
+    }
+    while (true);
+}
+finally
+{
+    Console.CursorVisible = true;
+    Console.Clear();
+}
